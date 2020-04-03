3119f197c96c87360d706c1104500f18309ca420
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-04-03 15:26:36 +0200

    update db model and data

--- db-bootstrap.sql
+++ db-bootstrap.sql
@@ -1,13 +1,52 @@
-INSERT INTO public.peer (id, public_key) VALUES (1, 'uBY9wEYelwood2z4FjC8zNZy2LN/WY17/GVOrZEhyWE=');
-INSERT INTO public.peer (id, public_key) VALUES (2, 'rwhW6Z1hxejVjfiaVXHWkb+kCYuflzv0zbSPPynjPXs=');
-INSERT INTO public.peer (id, public_key) VALUES (3, '55h6JA2ZMPzaoa+iZU62JmqmtgK3ydj4YdT9HkkhnEQ=');
-INSERT INTO public.peer (id, public_key) VALUES (4, 'i5AmQLLlPa4fQmfuHj7COCFwmwegI39WMfs/LIdzbFo=');
-INSERT INTO public.client (peer_id, serial, psk, healthy, last_check) VALUES (1, 'serial1', 'psk1', true, '2020-03-26 12:26:32.000000');
-INSERT INTO public.client (peer_id, serial, psk, healthy, last_check) VALUES (2, 'serial2', 'psk2', false, '2020-03-26 12:26:32.000000');
-INSERT INTO public.gateway (peer_id, id, access_group_id, endpoint) VALUES (3, 1, '1234-asdf-aad', '35.228.118.232:51820');
-INSERT INTO public.gateway (peer_id, id, access_group_id, endpoint) VALUES (4, 2, '1234-asdf-aad', '35.228.78.29:51820');
-INSERT INTO public.ip (peer_id, ip) VALUES (1, '10.255.248.4');
-INSERT INTO public.ip (peer_id, ip) VALUES (2, '10.255.248.5');
-INSERT INTO public.ip (peer_id, ip) VALUES (3, '10.255.248.2');
-INSERT INTO public.ip (peer_id, ip) VALUES (4, '10.255.248.3');
-INSERT INTO public.routes (gateway_id, cidr) VALUES (1, '1.2.3.4/23');
+/* apiserver */
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (1, 'FUwVtyvs8nIRx9RpUUEopkfV8idmHz9g9K/vf9MFOXI=', '10.255.240.1', 'control');
+
+/* vegar */
+INSERT INTO public.client (id, serial, psk, healthy, last_check)
+VALUES (1, 'serial1', 'psk1', true, '2020-03-26 12:26:32.000000');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (3, 'EatjldYVvB91aep5kxDnYsQ37Ufk92IBBIcfma1fzAs=', '10.255.240.2', 'control');
+INSERT INTO public.client_peer(client_id, peer_id)
+VALUES (1, 3);
+/* INSERT INTO public.peer (id, public_key, ip, type) VALUES (4, '', '10.255.248.2', 'data'); */
+/* INSERT INTO public.client_peer(client_id, peer_id) VALUES (1, 4); */
+
+/* johnny */
+INSERT INTO public.client (id, serial, psk, healthy, last_check)
+VALUES (2, 'serial2', 'psk2', true, '2020-03-26 12:26:32.000000');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (5, '', '10.255.240.3', 'control');
+INSERT INTO public.client_peer(client_id, peer_id)
+VALUES (2, 5);
+/* INSERT INTO public.peer (id, public_key, ip, type) VALUES (6, '', '10.255.248.3', 'data'); */
+/* INSERT INTO public.client_peer(client_id, peer_id) VALUES (2, 6); */
+
+
+/* gateway 1 */
+INSERT INTO public.gateway (id, access_group_id, endpoint)
+VALUES (1, '1234-asdf-aad1', '35.228.118.232:51820');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (7, 'QFwvy4pUYXpYm4z9iXw1GZRgjp3iU+3Hsu0UUvre9FM=', '10.255.240.4', 'control');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (8, '55h6JA2ZMPzaoa+iZU62JmqmtgK3ydj4YdT9HkkhnEQ=', '10.255.248.4', 'data');
+INSERT INTO public.gateway_peer(gateway_id, peer_id)
+VALUES (1, 7);
+INSERT INTO public.gateway_peer(gateway_id, peer_id)
+VALUES (1, 8);
+
+/* gateway 2 */
+INSERT INTO public.gateway (id, access_group_id, endpoint)
+VALUES (2, '1234-asdf-aad2', '35.228.118.232:51820');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (9, 'Whbuh2+T8/m1kJTtByfYQvlD/Efv4xxX9rbe9B2SK2M=', '10.255.240.5', 'control');
+INSERT INTO public.peer (id, public_key, ip, type)
+VALUES (10, 'i5AmQLLlPa4fQmfuHj7COCFwmwegI39WMfs/LIdzbFo=', '10.255.248.5', 'data');
+INSERT INTO public.gateway_peer(gateway_id, peer_id)
+VALUES (2, 9);
+INSERT INTO public.gateway_peer(gateway_id, peer_id)
+VALUES (2, 10);
+
+INSERT INTO public.routes (gateway_id, cidr)
+VALUES (1, '1.2.3.4/23');
+
