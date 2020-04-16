408fe1296a278be4ddf02ac834e9b8277e5e0e3d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-04-16 16:34:14 +0200

    remove data/control plane concept from gw agent

--- cmd/gateway-agent/main.go
+++ cmd/gateway-agent/main.go
@@ -6,11 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/nais/device/apiserver/api"
 	log "github.com/sirupsen/logrus"
 	flag "github.com/spf13/pflag"
 )
@@ -24,12 +24,15 @@ func init() {
 	flag.StringVar(&cfg.Apiserver, "apiserver", cfg.Apiserver, "hostname to apiserver")
 	flag.StringVar(&cfg.Name, "name", cfg.Name, "gateway name")
 	flag.StringVar(&cfg.PublicKey, "public-key", cfg.PublicKey, "path to wireguard public key")
-	flag.StringVar(&cfg.PrivateKey, "private-key", cfg.PrivateKey, "path to wireguard private key")
-	flag.StringVar(&cfg.TunnelInterfaceName, "interface", cfg.TunnelInterfaceName, "name of tunnel interface")
+	flag.StringVar(&cfg.PrivateKeyPath, "private-key", cfg.PrivateKeyPath, "path to wireguard private key")
+	flag.StringVar(&cfg.Interface, "interface", cfg.Interface, "name of tunnel interface")
 	flag.StringVar(&cfg.TunnelConfigDir, "tunnel-config-dir", cfg.TunnelConfigDir, "path to tunnel interface config directory")
 
 	flag.Parse()
+
+	cfg.WireGuardConfigPath = path.Join("/etc/wireguard/", fmt.Sprintf("%s.connf", cfg.Interface))
 }
+
 
 // Gateway agent ensures desired configuration as defined by the apiserver
 // is synchronized and enforced by the local wireguard process on the gateway.
@@ -46,11 +49,20 @@ func init() {
 //# ip link add dev wg0 type wireguard
 //# ip addr add <tunnelip> wg0
 //# ip link set wg0 up
+type Device struct {
+	Serial    string     `json:"serial"`
+	PSK       string     `json:"psk"`
+	LastCheck *time.Time `json:"lastCheck"`
+	Healthy   *bool      `json:"isHealthy"`
+	PublicKey string     `json:"publicKey"`
+	IP        string     `json:"ip"`
+}
+
 func main() {
 	log.Info("starting gateway-agent")
 	log.Infof("with config:\n%+v", cfg)
 
-	privateKey, err := readPrivateKey()
+	privateKey, err := readPrivateKey(cfg.PrivateKeyPath)
 
 	if err != nil {
 		log.Fatalf("reading private key: %s", err)
@@ -59,36 +71,36 @@ func main() {
 	for range time.NewTicker(10 * time.Second).C {
 		log.Infof("getting config")
 		apiserver := fmt.Sprintf("%s/gateways/%s", cfg.Apiserver, cfg.Name)
-		peers, err := getPeers(apiserver)
+		devices, err := getDevices(apiserver)
 		if err != nil {
 			log.Error(err)
 			// inc metric
 			continue
 		}
 
-		fmt.Printf("%+v\n", peers)
+		fmt.Printf("%+v\n", devices)
 
-		if err := configureWireguard(peers, privateKey); err != nil {
+		if err := configureWireguard(devices, cfg, privateKey); err != nil {
 			log.Error(err)
 			// inc metric
 		}
 	}
 }
 
-func readPrivateKey() (string, error) {
-	b, err := ioutil.ReadFile(cfg.PrivateKey)
+func readPrivateKey(privateKeyPath string) (string, error) {
+	b, err := ioutil.ReadFile(privateKeyPath)
 	return string(b), err
 }
 
-func configureWireguard(peers []api.Peer, privateKey string) error {
-	wgConfigContent := generateWGConfig(peers, privateKey)
+func configureWireguard(devices []Device, cfg Config, privateKey string) error {
+	wgConfigContent := generateWGConfig(devices, privateKey)
 	fmt.Println(string(wgConfigContent))
-	wgConfigFilePath := filepath.Join(cfg.TunnelConfigDir, cfg.TunnelInterfaceName+".conf")
+	wgConfigFilePath := filepath.Join(cfg.TunnelConfigDir, cfg.Interface+".conf")
 	if err := ioutil.WriteFile(wgConfigFilePath, wgConfigContent, 0600); err != nil {
 		return fmt.Errorf("writing wireguard config to disk: %w", err)
 	}
 
-	cmd := exec.Command("/usr/bin/wg", "syncconf", "wgdata", "/etc/wireguard/wgdata.conf")
+	cmd := exec.Command("/usr/bin/wg", "syncconf", cfg.Interface, cfg.WireGuardConfigPath)
 	if stdout, err := cmd.Output(); err != nil {
 		return fmt.Errorf("executing %w: %v", err, string(stdout))
 	}
@@ -96,11 +108,11 @@ func configureWireguard(peers []api.Peer, privateKey string) error {
 	return nil
 }
 
-func generateWGConfig(peers []api.Peer, privateKey string) []byte {
+func generateWGConfig(devices []Device, privateKey string) []byte {
 	wgConfig := "[Interface]\n"
-	wgConfig += fmt.Sprintf("PrivateKey = %s\n", strings.Trim(privateKey, "\n"))
+	wgConfig += fmt.Sprintf("PrivateKeyPath = %s\n", strings.Trim(privateKey, "\n"))
 	wgConfig += "ListenPort = 51820\n"
-	for _, peer := range peers {
+	for _, peer := range devices {
 		wgConfig += "[Peer]\n"
 		wgConfig += fmt.Sprintf("PublicKey = %s\n", peer.PublicKey)
 		wgConfig += fmt.Sprintf("AllowedIPs = %s\n\n", peer.IP)
@@ -109,39 +121,41 @@ func generateWGConfig(peers []api.Peer, privateKey string) []byte {
 	return []byte(wgConfig)
 }
 
-func getPeers(apiserverURL string) (peers []api.Peer, err error) {
-	resp, err := http.Get(apiserverURL)
+func getDevices(apiServerURL string) ([]Device, error) {
+	resp, err := http.Get(apiServerURL)
 	if err != nil {
 		return nil, fmt.Errorf("getting peer config from apiserver: %w", err)
 	}
 
 	defer resp.Body.Close()
 
-	err = json.NewDecoder(resp.Body).Decode(&peers)
+	var devices []Device
+	err = json.NewDecoder(resp.Body).Decode(&devices)
 
 	if err != nil {
 		return nil, fmt.Errorf("unmarshal json from apiserver: %w", err)
 	}
 
-	return
+	return devices, nil
 }
 
 type Config struct {
 	Apiserver           string
 	Name                string
 	PublicKey           string
-	PrivateKey          string
+	PrivateKeyPath      string
 	TunnelIP            string
 	TunnelConfigDir     string
-	TunnelInterfaceName string
+	Interface           string
+	WireGuardConfigPath string
 }
 
 func DefaultConfig() Config {
 	return Config{
-		Apiserver:           "http://apiserver.device.nais.io",
-		PublicKey:           "/etc/wireguard/public.key",
-		PrivateKey:          "/etc/wireguard/private.key",
-		TunnelInterfaceName: "wgdata",
-		TunnelConfigDir:     "/etc/wireguard/",
+		Apiserver:       "http://apiserver.device.nais.io",
+		PublicKey:       "/etc/wireguard/public.key",
+		PrivateKeyPath:  "/etc/wireguard/private.key",
+		Interface:       "wgdata",
+		TunnelConfigDir: "/etc/wireguard/",
 	}
 }
