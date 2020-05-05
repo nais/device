ec39b0347eda49478fc4aaa10286b3fabc03687a
Johnny Horvi <johnny@horvi.no>
2020-05-05 10:27:03 +0200

    added terraform setup for kolide-api-token, use right format for api-key

--- terraform/apiserver/vm.tf
+++ terraform/apiserver/vm.tf
@@ -6,8 +6,46 @@ resource "google_compute_address" "apiserver" {
   name = "apiserver"
 }
 
+resource "google_secret_manager_secret" "device-health-checker-apiserver-password" {
+  provider = google-beta
+
+  project   = "nais-device"
+  secret_id = "nais-device_device-health-checker_api-server-password"
+
+  labels = {
+    type      = "api-server-password"
+    component = "device-health-checker"
+  }
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "apiserver_access" {
+  provider = google-beta
+
+  project   = "nais-device"
+  secret_id = google_secret_manager_secret.device-health-checker-apiserver-password.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.apiserver.email}"
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+resource "google_secret_manager_secret_version" "access" {
+  provider = google-beta
+
+  secret      = google_secret_manager_secret.device-health-checker-apiserver-password.id
+  secret_data = random_password.password.result
+}
+
 locals {
-  secrets = toset(["slack-token", "database-uri", "device-health-checker"])
+  secrets = toset(["slack-token", "database-uri", "kolide-api-token"])
 }
 
 resource "google_secret_manager_secret" "secret" {
