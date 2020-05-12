2901fca56adcad8c3adfe655e5e82e20d6d30bef
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-05-12 16:14:20 +0200

    fix get serial for windows, add trimming

--- device-agent/serial/serial_windows.go
+++ device-agent/serial/serial_windows.go
@@ -1,18 +1,19 @@
 package serial
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
-	"strings"
 )
 
 func GetDeviceSerial() (string, error) {
 	cmd := exec.Command("wmic", "bios", "get", "serialnumber")
 	b, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", fmt.Errorf("getting serial with ioreg: %w: %v", err, string(b))
+		return "", fmt.Errorf("getting serial with wmic: %w: %v", err, string(b))
 	}
 
-	lines := strings.Split(string(b), "\n")
-	return lines[1], nil
+	lines := bytes.Split(b, []byte("\r\r\n"))
+	trimmed := bytes.TrimSpace(lines[1])
+	return string(trimmed), nil
 }
