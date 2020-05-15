070df5d5aeb28dffd09ed02fbe672ebee4072f7b
Christer Edvartsen <christer.edvartsen@nav.no>
2020-05-15 08:11:07 +0200

    Add exception handler and make sure the app does not render exceptions its own way

--- device-health-checker/device-health-checker.php
+++ device-health-checker/device-health-checker.php
@@ -1,11 +1,23 @@
 <?php
 namespace Nais\Device;
 
+use Symfony\Component\Console\Application;
+use Throwable;
+
+set_exception_handler(function(Throwable $e) : void {
+    echo json_encode([
+        'component' => 'device-health-checker',
+        'system'    => 'nais-device',
+        'message'   => $e->getMessage(),
+        'timestamp' => time(),
+    ]) . PHP_EOL;
+    exit(1);
+});
+
 require 'vendor/autoload.php';
 
-use Symfony\Component\Console\Application;
-
 $application = new Application('Device health checker');
+$application->setCatchExceptions(false);
 $application->add(new Command\ListChecks());
-$application->add(new Command\CheckAndUpdateDevices(require 'checks-config.php'));
+$application->add(new Command\CheckAndUpdateDevices());
 $application->run();
