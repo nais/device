7b13dac7e57a99ef3cf9df148b5662e263450e88
Christer Edvartsen <christer.edvartsen@nav.no>
2020-05-15 08:09:53 +0200

    Add criticality level

--- device-health-checker/src/Criticality.php
+++ device-health-checker/src/Criticality.php
@@ -2,7 +2,8 @@
 namespace Nais\Device;
 
 class Criticality {
-    const LOW  = 604800; // 7 days
-    const MED  = 172800; // 2 days
-    const HIGH = 3600;   // 1 hour
+    const IGNORE = -1;     // Used for checks we want to ignore
+    const LOW    = 604800; // 7 days
+    const MED    = 172800; // 2 days
+    const HIGH   = 3600;   // 1 hour
 }
