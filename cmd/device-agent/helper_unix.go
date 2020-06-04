4d5c5d169b0b899e8667fa4b16063500f57a2225
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-06-04 14:20:46 +0200

    Windows device-agent run as normal user #39
    Prompting for admin when running helper, by ading a manifest to
    device-agent-helper.exe

--- cmd/device-agent/helper_unix.go
+++ cmd/device-agent/helper_unix.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"os"
+	"os/exec"
 
 	"github.com/nais/device/device-agent/runtimeconfig"
 )
@@ -22,4 +23,8 @@ func runHelper(rc *runtimeconfig.RuntimeConfig, ctx context.Context) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Start()
+}
+
+func adminCommandContext(ctx context.Context, command string, arg ...string) *exec.Cmd {
+	return exec.CommandContext(ctx, "sudo", append([]string{command}, arg...)...)
 }
