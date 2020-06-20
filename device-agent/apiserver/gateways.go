f80e423052e53fb6518e8050570c8860ebb885ed
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-06-20 14:35:58 +0200

    Update gateways.go

--- device-agent/apiserver/gateways.go
+++ device-agent/apiserver/gateways.go
@@ -50,9 +50,12 @@ Endpoint = %s
 `
 	allowedIPs := make([]string, 0)
 	allowedIPs = append(allowedIPs, gw.IP+"/32")
+	/*
 	if gw.Healthy == true {
 		allowedIPs = append(allowedIPs, gw.Routes...)
 	}
+	*/
+	allowedIPs = append(allowedIPs, gw.Routes...) // TODO: Remove when health checks are fixed
 
 	s := fmt.Sprintf(peerTemplate, gw.PublicKey, strings.Join(allowedIPs, ","), gw.Endpoint)
 	return []byte(s)
