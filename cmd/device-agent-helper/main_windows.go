a01cec409314212ae9ed3c06fc5eea2b7627cea1
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-06-25 14:43:25 +0200

    fix interfaceExists check

--- cmd/device-agent-helper/main_windows.go
+++ cmd/device-agent-helper/main_windows.go
@@ -65,22 +65,24 @@ func platformInit(cfg *Config) {
 func interfaceExists(ctx context.Context, cfg Config) bool {
 	queryService := exec.CommandContext(ctx, "sc", "query", serviceName(cfg.Interface))
 	if err := queryService.Run(); err != nil {
-		return true
-	} else {
 		return false
+	} else {
+		return true
 	}
 }
 
 func setupInterface(ctx context.Context, cfg Config) error {
-	if !interfaceExists(ctx, cfg) {
-		installService := exec.CommandContext(ctx, WireGuardBinary, "/installtunnelservice", cfg.WireGuardConfigPath)
-		if b, err := installService.CombinedOutput(); err != nil {
-			return fmt.Errorf("installing tunnel service: %v: %v", err, string(b))
-		} else {
-			log.Infof("installed tunnel service, sleeping 3 sec to let it finish")
-			time.Sleep(3 * time.Second)
-			log.Infof("resuming")
-		}
+	if interfaceExists(ctx, cfg) {
+		return nil
+	}
+
+	installService := exec.CommandContext(ctx, WireGuardBinary, "/installtunnelservice", cfg.WireGuardConfigPath)
+	if b, err := installService.CombinedOutput(); err != nil {
+		return fmt.Errorf("installing tunnel service: %v: %v", err, string(b))
+	} else {
+		log.Infof("installed tunnel service, sleeping 3 sec to let it finish")
+		time.Sleep(3 * time.Second)
+		log.Infof("resuming")
 	}
 
 	return nil
@@ -109,15 +111,18 @@ func syncConf(cfg Config, ctx context.Context) error {
 }
 
 func teardownInterface(ctx context.Context, cfg Config) {
-	if interfaceExists(ctx, cfg) {
-		uninstallService := exec.CommandContext(ctx, WireGuardBinary, "/uninstalltunnelservice", cfg.Interface)
-		if b, err := uninstallService.CombinedOutput(); err != nil {
-			log.Warnf("uninstalling tunnel service: %v: %v", err, string(b))
-		} else {
-			log.Infof("uninstalled tunnel service (sleeping 3 sec to let it finish)")
-			time.Sleep(3 * time.Second)
-			log.Infof("resuming")
-		}
+	if !interfaceExists(ctx, cfg) {
+		log.Info("no interface")
+		return
+	}
+
+	uninstallService := exec.CommandContext(ctx, WireGuardBinary, "/uninstalltunnelservice", cfg.Interface)
+	if b, err := uninstallService.CombinedOutput(); err != nil {
+		log.Warnf("uninstalling tunnel service: %v: %v", err, string(b))
+	} else {
+		log.Infof("uninstalled tunnel service (sleeping 3 sec to let it finish)")
+		time.Sleep(3 * time.Second)
+		log.Infof("resuming")
 	}
 }
 
