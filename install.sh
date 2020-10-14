844f1e05932150d209528a17b5e2af2a8e227d3f
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2020-10-14 10:48:44 +0200

    use same colors as previously

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ err='no error'
 latest_tag=''
 
 ok() {
-  echo -e "..[\033[0;32mok\033[0m]"
+  echo -e "..[\033[38;5;82mok\033[0;5;82m]"
 }
 
 fail() {
-  echo -e "[\033[0;31mfail\033[0m]"
+  echo -e "[\033[31;5;82mfail\033[0;5;82m]"
   echo $err
   exit 1
 }
