d86cd599c75437c33809e031c9d12c9c67a3b7f0
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-11-05 11:43:15 +0100

    cleanup old unused files

--- setup-gcp.sh
+++
@@ -1,4 +0,0 @@
-gcloud beta compute --project=nais-device instances create gateway-1 --zone=europe-north1-a --machine-type=f1-micro --tags=wireguard-gateway --image=ubuntu-1804-bionic-v20200317 --image-project=ubuntu-os-cloud
-
-gcloud compute --project=nais-device firewall-rules create allow-wireguard --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=udp:51820 --source-ranges=0.0.0.0/0 --target-tags=wireguard-gateway # wireguard firewall
-
