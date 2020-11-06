f64c1ea732e7a4937785e9efc483f958e078225f
Christer Edvartsen <christer.edvartsen@nav.no>
2020-11-06 15:16:31 +0100

    Remove main component directory

--- device-health-checker/src/Exception/MultipleKolideDevicesException.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types=1);
-namespace Nais\Device\Exception;
-
-use RuntimeException;
-
-class MultipleKolideDevicesException extends RuntimeException implements HealthCheckerException {}
