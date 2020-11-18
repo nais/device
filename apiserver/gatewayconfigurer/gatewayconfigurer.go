3bd482afdb13621fd7b4f8d0809d8e6e7aa9c529
Johnny Horvi <johnny@horvi.no>
2020-11-18 21:31:33 +0100

    enable reading gw config from bucket

--- apiserver/gatewayconfigurer/gatewayconfigurer.go
+++ apiserver/gatewayconfigurer/gatewayconfigurer.go
@@ -54,10 +54,9 @@ func (g *GatewayConfigurer) SyncConfig(ctx context.Context) error {
 	}
 
 	for gatewayName, gatewayConfig := range gatewayConfigs {
-		log.Infof("### Would update gateway: %s with routes: %s and accessGroupIds: %s: %v", gatewayName, gatewayConfig.Routes, gatewayConfig.AccessGroupIds, err)
-		//if err := g.DB.UpdateGateway(context.Background(), gatewayName, ToCIDRStringSlice(gatewayConfig.Routes), gatewayConfig.AccessGroupIds); err != nil {
-		//	return fmt.Errorf("updating gateway: %s with routes: %s and accessGroupIds: %s: %v", gatewayName, gatewayConfig.Routes, gatewayConfig.AccessGroupIds, err)
-		//}
+		if err := g.DB.UpdateGateway(context.Background(), gatewayName, ToCIDRStringSlice(gatewayConfig.Routes), gatewayConfig.AccessGroupIds); err != nil {
+			return fmt.Errorf("updating gateway: %s with routes: %s and accessGroupIds: %s: %v", gatewayName, gatewayConfig.Routes, gatewayConfig.AccessGroupIds, err)
+		}
 	}
 
 	return nil
--- apiserver/gatewayconfigurer/gatewayconfigurer_test.go
+++ apiserver/gatewayconfigurer/gatewayconfigurer_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/nais/device/apiserver/gatewayconfigurer"
+	"github.com/nais/device/apiserver/testdatabase"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"os"
@@ -16,53 +17,53 @@ func TestGatewayConfigurer_SyncConfig(t *testing.T) {
 		t.Skip("Skipping integration test")
 	}
 
-	//t.Run("updates gateway config in database according to bucket definition", func(t *testing.T) {
-	//	testDB, err := testdatabase.New("user=postgres password=postgres host=localhost port=5433 sslmode=disable", "../database/schema/schema.sql")
-	//	assert.NoError(t, err)
-	//	const gatewayName, route, accessGroupId = "name", "r", "agid"
-	//	assert.NoError(t, testDB.AddGateway(context.Background(), gatewayName, "", ""))
-	//
-	//	bucketReader := MockBucketReader{GatewayConfigs: gatewayConfig(gatewayName, route, accessGroupId)}
-	//
-	//	gc := gatewayconfigurer.GatewayConfigurer{
-	//		DB:           testDB,
-	//		BucketReader: bucketReader,
-	//	}
-	//
-	//	gateway, err := testDB.ReadGateway(gatewayName)
-	//	assert.NoError(t, err)
-	//	assert.Equal(t, gatewayName, gateway.Name)
-	//	assert.Nil(t, gateway.Routes)
-	//	assert.Nil(t, gateway.AccessGroupIDs)
-	//
-	//	assert.NoError(t, gc.SyncConfig(context.Background()))
-	//
-	//	updatedGateway, err := testDB.ReadGateway(gatewayName)
-	//	assert.NoError(t, err)
-	//	assert.Len(t, updatedGateway.Routes, 1)
-	//	assert.Equal(t, route, updatedGateway.Routes[0])
-	//	assert.Len(t, updatedGateway.AccessGroupIDs, 1)
-	//	assert.Equal(t, accessGroupId, updatedGateway.AccessGroupIDs[0])
-	//})
-	//
-	//t.Run("synchronizing gatewayconfig where gateway not in database is ok", func(t *testing.T) {
-	//	testDB, err := testdatabase.New("user=postgres password=postgres host=localhost port=5433 sslmode=disable", "../database/schema/schema.sql")
-	//	assert.NoError(t, err)
-	//	const gatewayName, route, accessGroupId = "name", "r", "agid"
-	//
-	//	bucketReader := MockBucketReader{GatewayConfigs: gatewayConfig(gatewayName, route, accessGroupId)}
-	//
-	//	gc := gatewayconfigurer.GatewayConfigurer{
-	//		DB:           testDB,
-	//		BucketReader: bucketReader,
-	//	}
-	//
-	//	gw, err := testDB.ReadGateway(gatewayName)
-	//	assert.Error(t, err)
-	//	assert.Nil(t, gw)
-	//
-	//	assert.NoError(t, gc.SyncConfig(context.Background()))
-	//})
+	t.Run("updates gateway config in database according to bucket definition", func(t *testing.T) {
+		testDB, err := testdatabase.New("user=postgres password=postgres host=localhost port=5433 sslmode=disable", "../database/schema/schema.sql")
+		assert.NoError(t, err)
+		const gatewayName, route, accessGroupId = "name", "r", "agid"
+		assert.NoError(t, testDB.AddGateway(context.Background(), gatewayName, "", ""))
+
+		bucketReader := MockBucketReader{GatewayConfigs: gatewayConfig(gatewayName, route, accessGroupId)}
+
+		gc := gatewayconfigurer.GatewayConfigurer{
+			DB:           testDB,
+			BucketReader: bucketReader,
+		}
+
+		gateway, err := testDB.ReadGateway(gatewayName)
+		assert.NoError(t, err)
+		assert.Equal(t, gatewayName, gateway.Name)
+		assert.Nil(t, gateway.Routes)
+		assert.Nil(t, gateway.AccessGroupIDs)
+
+		assert.NoError(t, gc.SyncConfig(context.Background()))
+
+		updatedGateway, err := testDB.ReadGateway(gatewayName)
+		assert.NoError(t, err)
+		assert.Len(t, updatedGateway.Routes, 1)
+		assert.Equal(t, route, updatedGateway.Routes[0])
+		assert.Len(t, updatedGateway.AccessGroupIDs, 1)
+		assert.Equal(t, accessGroupId, updatedGateway.AccessGroupIDs[0])
+	})
+
+	t.Run("synchronizing gatewayconfig where gateway not in database is ok", func(t *testing.T) {
+		testDB, err := testdatabase.New("user=postgres password=postgres host=localhost port=5433 sslmode=disable", "../database/schema/schema.sql")
+		assert.NoError(t, err)
+		const gatewayName, route, accessGroupId = "name", "r", "agid"
+
+		bucketReader := MockBucketReader{GatewayConfigs: gatewayConfig(gatewayName, route, accessGroupId)}
+
+		gc := gatewayconfigurer.GatewayConfigurer{
+			DB:           testDB,
+			BucketReader: bucketReader,
+		}
+
+		gw, err := testDB.ReadGateway(gatewayName)
+		assert.Error(t, err)
+		assert.Nil(t, gw)
+
+		assert.NoError(t, gc.SyncConfig(context.Background()))
+	})
 
 }
 
