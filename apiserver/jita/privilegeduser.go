6e6043d6ebf0cd49c0959671f566b727cdefef97
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2020-11-26 15:04:55 +0100

    handles !200 and use correct username
    Co-authored-by: Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
    Co-authored-by: Christer Edvartsen <christer.edvartsen@nav.no>

--- apiserver/jita/privilegeduser.go
+++ apiserver/jita/privilegeduser.go
@@ -3,6 +3,7 @@ package jita
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
 type PrivilegedUser struct {
@@ -14,6 +15,9 @@ func (j *Jita) GetPrivilegedUsersForGateway(gateway string) ([]PrivilegedUser, e
 	resp, err := j.HTTPClient.Get(fmt.Sprintf("%s/%s/%s", j.Url, "gatewayAccess", gateway))
 	if err != nil {
 		return nil, fmt.Errorf("getting privileged users: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("not ok when calling jita: %v", resp.StatusCode)
 	}
 	var users []PrivilegedUser
 	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
