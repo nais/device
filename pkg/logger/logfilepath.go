e15f9bea6c756fd7606dd03804d3afec9bd9eae2
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-01-12 15:49:28 +0100

    Fix buggy logdir creation

--- pkg/logger/logfilepath.go
+++ pkg/logger/logfilepath.go
@@ -12,12 +12,25 @@ import (
 )
 
 func DeviceAgentLogFilePath(configDir string) string {
-	logFilePath := filepath.Join(configDir, "logs", "device-agent.log")
-	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
-		err = os.MkdirAll(logFilePath, 0755)
+	logPath := filepath.Join(configDir, "logs")
+	if _, err := os.Stat(logPath); os.IsNotExist(err) {
+		err = os.MkdirAll(logPath, 0755)
 		if err != nil {
 			log.Fatalf("Unable to create logs directory: %v", err)
 		}
 	}
+	logFilePath := filepath.Join(logPath, "device-agent.log")
+	cleanUp(logFilePath)
 	return logFilePath
+}
+
+// Clean up after the version that messed up by creating a directory inplace of the log file
+func cleanUp(logFilePath string) {
+	fileInfo, err := os.Stat(logFilePath)
+	if err == nil && fileInfo.IsDir() {
+		err = os.RemoveAll(logFilePath)
+		if err != nil {
+			log.Fatalf("Failed to remove directory: %v", err)
+		}
+	}
 }
