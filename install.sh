c9437d7a62d6e0a80f4c2c07415a4cc9cd7c32af
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-01-14 12:43:31 +0100

    Make install script support linux

--- install.sh
+++ install.sh
@@ -14,6 +14,28 @@ fail() {
   exit 1
 }
 
+case "$(uname -s)" in
+   Darwin)
+     installer_ext=pkg
+     install() {
+       /usr/bin/osascript -e "do shell script \"pkill device-agent; pkill device-agent-helper; installer -target / -pkg '$temp_installer'\" with prompt \"naisdevice wonders if anyone ever reads this message? also it would like to install itself 😎\" with administrator privileges"
+     }
+     ;;
+
+   Linux)
+     installer_ext=deb
+     install() {
+       pkill naisdevice
+       pkexec dpkg --install "${temp_installer}"
+     }
+     ;;
+
+   *)
+     err="This install script does not support your OS :("
+     fail
+     ;;
+esac
+
 echo "##################################"
 echo "# Installing naisdevice           "
 echo "##################################"
@@ -22,10 +44,10 @@ echo
 echo -n "determining latest version..."
 latest_tag=$(curl --show-error --silent --fail -L "https://api.github.com/repos/nais/device/releases/latest" | grep 'tag_name' | sed -E 's/.*"([^"]+)".*/\1/') && ok || fail
 
-echo -n "downloading latest pkg......."
-pkg_url="https://github.com/nais/device/releases/download/${latest_tag}/naisdevice.pkg"
-temp_pkg=$(mktemp).pkg
-err=$(curl --show-error --silent --fail -L "$pkg_url" -o "$temp_pkg") && ok || fail
+echo -n "downloading latest installer......."
+installer_url="https://github.com/nais/device/releases/download/${latest_tag}/naisdevice.${installer_ext}"
+temp_installer="$(mktemp).${installer_ext}"
+err=$(curl --show-error --silent --fail -L "$installer_url" -o "$temp_installer") && ok || fail
 
 echo -n "installing package..........."
-err=$(/usr/bin/osascript -e "do shell script \"pkill device-agent; pkill device-agent-helper; installer -target / -pkg '$temp_pkg'\" with prompt \"naisdevice wonders if anyone ever reads this message? also it would like to install itself 😎\" with administrator privileges") && ok || fail
+err=$(install) && ok || fail
