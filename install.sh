c9fae402efacc23a6b6a6cad95b5fd13b09f7223
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-01-26 15:11:45 +0100

    Add information about tag being installed

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ echo
 echo -n "determining latest version..."
 latest_tag=$(curl --show-error --silent --fail --location "https://api.github.com/repos/nais/device/releases/latest" | grep 'tag_name' | sed -E 's/.*"([^"]+)".*/\1/') && ok || fail
 
-echo -n "downloading latest installer......."
+echo -n "downloading latest installer (${latest_tag})......."
 installer_url="https://github.com/nais/device/releases/download/${latest_tag}/naisdevice.${installer_ext}"
 temp_installer="$(mktemp).${installer_ext}"
 err=$(curl --show-error --silent --fail --location "$installer_url" --output "$temp_installer") && ok || fail
