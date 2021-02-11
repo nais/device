341cf7cd9492e185518fccfde3b47acb1ff6783e
Kim Tore Jensen <kimtjen@gmail.com>
2021-02-11 16:23:55 +0100

    windows: import package time

--- pkg/device-agent/const_windows.go
+++ pkg/device-agent/const_windows.go
@@ -1,4 +1,8 @@
 package device_agent
 
+import (
+	"time"
+)
+
 // device-helper on Windows needs a _lot_ of time to configure the interface for the first time.
 const helperTimeout = 15 * time.Second
