4e4cdbd3b87e5e0f4a1eb2e844e66937c16f9e69
Per Christian Moen <per.christian.moen@nav.no>
2021-05-07 00:29:10 +0200

    Add support for black and white icons
    This is controlled by a setting. Default value is off.

--- pkg/systray/systray.go
+++ pkg/systray/systray.go
@@ -1,6 +1,10 @@
 package systray
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
 	"github.com/getlantern/systray"
 
 	"google.golang.org/grpc"
@@ -18,11 +22,14 @@ type Config struct {
 	LogFilePath string
 
 	AutoConnect bool
+	BlackAndWhiteIcons bool
 }
 
 var cfg Config
 
 var connection *grpc.ClientConn
+
+const ConfigFile = "/systray-config.json"
 
 func onReady() {
 	var err error
@@ -62,4 +69,31 @@ func Spawn(systrayConfig Config) {
 	cfg = systrayConfig
 
 	systray.Run(onReady, onExit)
+}
+
+
+func WriteToJSONFile(strct interface{}, path string) error {
+	b, err := json.Marshal(&strct)
+	if err != nil {
+		return fmt.Errorf("marshaling struct into json: %w", err)
+	}
+	if err := ioutil.WriteFile(path, b, 0600); err != nil {
+		return err
+	}
+
+	log.Infof("Wrote config %+v to %s", strct, path)
+	return nil
+}
+
+
+func ReadFromJSONFile(path string) (*Config, error) {
+	var config Config
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading systray config from disk: %w", err)
+	}
+	if err := json.Unmarshal(b, &config); err != nil {
+		return nil, fmt.Errorf("unmarshaling systray config: %w", err)
+	}
+	return &config, nil
 }
