32303714c68f8462bc95d2fdbe3a2d9a3ec136f6
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-06-24 15:43:41 +0200

    move more log debug

--- apiserver/api/api.go
+++ apiserver/api/api.go
@@ -89,7 +89,7 @@ func healthy(devices []database.Device) []database.Device {
 
 		if *device.Healthy {
 			if timeNow.After(kolideLastSeenDevice.Add(MaxTimeSinceKolideLastSeen)) {
-				log.Infof("Would have skipped device: %s with owner %s. (last seen: %s, now: %s).", device.Serial, device.Username, kolideLastSeenDevice, timeNow)
+				log.Debugf("Would have skipped device: %s with owner %s. (last seen: %s, now: %s).", device.Serial, device.Username, kolideLastSeenDevice, timeNow)
 			}
 
 			healthyDevices = append(healthyDevices, device)
