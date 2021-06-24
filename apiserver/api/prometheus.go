1891543d65144e45234956d4d2726c4fa2197fc5
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-06-24 15:49:48 +0200

    initialize metrics in tests
    make it impossible to initiaize metrics more than one time as this
    crashes

--- apiserver/api/prometheus.go
+++ apiserver/api/prometheus.go
@@ -11,6 +11,7 @@ var (
 	PrivilegedUsersPerGateway *prometheus.GaugeVec
 	DeviceConfigsReturned     *prometheus.CounterVec
 	GatewayConfigsReturned    *prometheus.CounterVec
+	initialized = false
 )
 
 func Serve(address string) {
@@ -19,13 +20,17 @@ func Serve(address string) {
 }
 
 func InitializeMetrics() {
+	if initialized {
+		return
+	}
+	initialized = true
+
 	PrivilegedUsersPerGateway = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "naisdevice",
 		Subsystem: "apiserver",
 		Name:      "privileged_users",
 		Help:      "privileged users per gateway",
 	}, []string{"gateway"})
-	prometheus.MustRegister(PrivilegedUsersPerGateway)
 
 	DeviceConfigsReturned = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace:   "naisdevice",
@@ -33,7 +38,6 @@ func InitializeMetrics() {
 		Name:        "user_configs_returned",
 		Help:        "Total number of configs returned to device since apiserver started.",
 	}, []string{"serial", "username"})
-	prometheus.MustRegister(DeviceConfigsReturned)
 
 	GatewayConfigsReturned = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "naisdevice",
@@ -41,5 +45,6 @@ func InitializeMetrics() {
 		Name:      "gateway_configs_returned",
 		Help:      "Total number of configs returned to gateway since apiserver started.",
 	}, []string{"gateway"})
-	prometheus.MustRegister(GatewayConfigsReturned)
+
+	prometheus.MustRegister(PrivilegedUsersPerGateway, DeviceConfigsReturned, GatewayConfigsReturned)
 }
