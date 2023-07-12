ae71ce2c6899ff2b3e5fdd6ff83f439f5335636d
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-07-12 12:53:20 +0200

    convert sessions as well

--- operational/scripts/convert_to_sqlite.go
+++ operational/scripts/convert_to_sqlite.go
@@ -8,75 +8,62 @@ import (
 	"strings"
 )
 
-func main() {
-	// generated with: psql "sslmode=disable host=$(pwd)/nais-device:europe-north1:naisdevice-3824b4c7 user=apiserver dbname=naisdevice" -o devices.csv --csv -c "select * from device"
-	devicesFile, err := os.Open("data/devices.csv")
+func convert(path string, convertFn func(map[string]string)) {
+	file, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("open: %v", err)
 		os.Exit(1)
 	}
 
-	devices := csv.NewReader(devicesFile)
-	deviceKeys, err := devices.Read()
+	csvReader := csv.NewReader(file)
+	csvHeader, err := csvReader.Read()
 	if err != nil {
 		fmt.Printf("get csv keys row: %v", err)
 		os.Exit(1)
 	}
 
 	for {
-		device, err := devices.Read()
+		line, err := csvReader.Read()
 		if err == io.EOF {
 			break
 		}
 
-		deviceAsMap := make(map[string]string)
-		for i, value := range device {
-			deviceAsMap[deviceKeys[i]] = value
+		data := make(map[string]string)
+		for i, value := range line {
+			data[csvHeader[i]] = value
 		}
 
-		convertDevice(deviceAsMap)
-	}
-
-	// generated with: psql "sslmode=disable host=$(pwd)/nais-device:europe-north1:naisdevice-3824b4c7 user=apiserver dbname=naisdevice" -o gateways.csv --csv -c "select * from gateway"
-	gatewaysFile, err := os.Open("data/gateways.csv")
-	if err != nil {
-		fmt.Printf("open: %v", err)
-		os.Exit(1)
-	}
-
-	gateways := csv.NewReader(gatewaysFile)
-	gatewayKeys, err := gateways.Read()
-	if err != nil {
-		fmt.Printf("get csv keys row: %v", err)
-		os.Exit(1)
-	}
-
-	for {
-		gateway, err := gateways.Read()
-		if err == io.EOF {
-			break
-		}
-
-		gatewayAsMap := make(map[string]string)
-		for i, value := range gateway {
-			gatewayAsMap[gatewayKeys[i]] = value
-		}
-
-		convertGateway(gatewayAsMap)
+		convertFn(data)
 	}
 }
 
-func convertDevice(device map[string]string) {
-	// sqlite> PRAGMA table_info(devices);
-	// 0|id|INTEGER|0||1
-	// 1|username|TEXT|1||0
-	// 2|serial|TEXT|1||0
-	// 3|platform|TEXT|1||0
-	// 4|healthy|BOOLEAN|1|0|0
-	// 5|last_updated|TEXT|0||0
-	// 6|public_key|TEXT|1||0
-	// 7|ip|TEXT|1||0
+func main() {
+	// generated with: psql "sslmode=disable host=$HOME/cloud_sql_sockets/nais-device:europe-north1:naisdevice-3824b4c7 user=apiserver dbname=naisdevice" -o devices.csv --csv -c "select * from device"
+	convert("data/devices.csv", convertDevice)
 
+	// generated with: psql "sslmode=disable host=$HOME/cloud_sql_sockets/nais-device:europe-north1:naisdevice-3824b4c7 user=apiserver dbname=naisdevice" -o gateways.csv --csv -c "select * from gateway"
+	convert("data/gateways.csv", convertGateway)
+
+	// generated with: psql "sslmode=disable host=$HOME/cloud_sql_sockets/nais-device:europe-north1:naisdevice-3824b4c7 user=apiserver dbname=naisdevice" -o sessions.csv --csv -c "select * from session"
+	convert("data/sessions.csv", convertSessions)
+}
+
+/*
+# source csv
+id,username,serial,psk,platform,healthy,public_key,ip,last_updated,kolide_last_seen
+
+# target tables
+sqlite> PRAGMA table_info(devices);
+0|id|INTEGER|0||1
+1|username|TEXT|1||0
+2|serial|TEXT|1||0
+3|platform|TEXT|1||0
+4|healthy|BOOLEAN|1|0|0
+5|last_updated|TEXT|0||0
+6|public_key|TEXT|1||0
+7|ip|TEXT|1||0
+*/
+func convertDevice(device map[string]string) {
 	fmt.Printf("INSERT INTO devices(id, username, serial, platform, healthy, last_updated, public_key, ip) VALUES(%s, '%s', '%s', '%s', %s, '%s', '%s', '%s');\n",
 		device["id"],
 		device["username"],
@@ -89,25 +76,28 @@ func convertDevice(device map[string]string) {
 	)
 }
 
+/*
+# source csv
+id,name,access_group_ids,endpoint,public_key,ip,routes,requires_privileged_access,password_hash
+
+# target tables
+sqlite> PRAGMA table_info(gateways);
+0|name|TEXT|0||1
+1|endpoint|TEXT|1||0
+2|public_key|TEXT|1||0
+3|ip|TEXT|1||0
+4|requires_privileged_access|BOOLEAN|1|0|0
+5|password_hash|TEXT|1||0
+
+sqlite> PRAGMA table_info(gateway_routes);
+0|gateway_name|TEXT|1||1
+1|route|TEXT|1||2
+
+sqlite> PRAGMA table_info(gateway_access_group_ids);
+0|gateway_name|TEXT|1||1
+1|group_id|TEXT|1||2
+*/
 func convertGateway(gateway map[string]string) {
-	// sqlite> PRAGMA table_info(gateways);
-	// 0|name|TEXT|0||1
-	// 1|endpoint|TEXT|1||0
-	// 2|public_key|TEXT|1||0
-	// 3|ip|TEXT|1||0
-	// 4|requires_privileged_access|BOOLEAN|1|0|0
-	// 5|password_hash|TEXT|1||0
-
-	// sqlite> PRAGMA table_info(gateway_routes);
-	// 0|gateway_name|TEXT|1||1
-	// 1|route|TEXT|1||2
-
-	// sqlite> PRAGMA table_info(gateway_access_group_ids);
-	// 0|gateway_name|TEXT|1||1
-	// 1|group_id|TEXT|1||2
-
-	// id,name,access_group_ids,endpoint,public_key,ip,routes,requires_privileged_access,password_hash
-
 	fmt.Printf("INSERT INTO gateways(name, endpoint, public_key, ip, requires_privileged_access, password_hash) VALUES('%s', '%s', '%s', '%s', %s, '%s');\n",
 		gateway["name"],
 		gateway["endpoint"],
@@ -138,6 +128,37 @@ func convertGateway(gateway map[string]string) {
 
 		fmt.Printf("INSERT INTO gateway_access_group_ids(gateway_name, group_id) VALUES('%s', '%s');\n",
 			gateway["name"],
+			groupID,
+		)
+	}
+}
+
+/*
+# source csv
+key,device_id,groups,object_id,expiry
+
+# target tables
+sqlite> PRAGMA table_info(sessions);
+0|key|TEXT|1||0
+1|expiry|TEXT|1||0
+2|device_id|INTEGER|1||0
+3|object_id|TEXT|1||0
+
+sqlite> PRAGMA table_info(session_access_group_ids);
+0|session_key|TEXT|1||1
+1|group_id|TEXT|1||2
+*/
+func convertSessions(sessions map[string]string) {
+	fmt.Printf("INSERT INTO sessions(key, expiry, device_id, object_id) VALUES('%s', '%s', %s, '%s');\n",
+		sessions["key"],
+		sessions["expiry"],
+		sessions["device_id"],
+		sessions["object_id"],
+	)
+
+	for _, groupID := range strings.Split(sessions["groups"], ",") {
+		fmt.Printf("INSERT INTO session_access_group_ids(session_key, group_id) VALUES('%s', '%s');\n",
+			sessions["key"],
 			groupID,
 		)
 	}
