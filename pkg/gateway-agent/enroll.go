4fc4ea86bb38559d4af8b5947c8ceb2d7777ed3b
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-08-10 10:04:43 +0200

    remove unused function persistPassword

--- pkg/gateway-agent/enroll.go
+++ pkg/gateway-agent/enroll.go
@@ -1,11 +1,5 @@
 package gateway_agent
 
-import (
-	"encoding/base64"
-	"fmt"
-	"os"
-)
-
 const (
 	DefaultConfigPath = "/etc/default/gateway-agent"
 )
@@ -18,20 +12,4 @@ func NewEnroller(cfg Config) Enroller {
 	return Enroller{
 		cfg: cfg,
 	}
-}
-
-func (e *Enroller) persistPassword(password []byte, file string) error {
-	fd, err := os.OpenFile(file, os.O_WRONLY|os.O_APPEND, 0o600)
-	if err != nil {
-		return fmt.Errorf("open file: %w", err)
-	}
-
-	encoded := base64.StdEncoding.EncodeToString(password)
-	_, err = fmt.Fprintf(fd, "GATEWAY_AGENT_APISERVERPASSWORD=\"%s\"\n", encoded)
-	if err != nil {
-		fd.Close()
-		return fmt.Errorf("append to file: %w", err)
-	}
-
-	return fd.Close()
 }
