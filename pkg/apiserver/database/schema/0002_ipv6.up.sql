95ee8a7dab9771c687e525e0aa7acd4905b1c605
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-09-20 14:49:14 +0200

    temporarily set ipv6 default to empty string

--- pkg/apiserver/database/schema/0002_ipv6.up.sql
+++ pkg/apiserver/database/schema/0002_ipv6.up.sql
@@ -1,7 +1,7 @@
-ALTER TABLE devices ADD COLUMN ipv6 TEXT NOT NULL;
+ALTER TABLE devices ADD COLUMN ipv6 TEXT NOT NULL DEFAULT '';
 ALTER TABLE devices RENAME COLUMN ip TO ipv4;
 CREATE INDEX devices_ipv6_idx ON devices (ipv6);
 
-ALTER TABLE gateways ADD COLUMN ipv6 TEXT NOT NULL;
+ALTER TABLE gateways ADD COLUMN ipv6 TEXT NOT NULL DEFAULT '';
 ALTER TABLE gateways RENAME COLUMN ip TO ipv4;
 CREATE INDEX gateways_ipv6_idx ON gateways (ipv6);
