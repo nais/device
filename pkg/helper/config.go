a899d936bd581f819f7cba99f75e332616fdb55b
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-11-02 12:15:32 +0100

    remove unused config field

--- pkg/helper/config.go
+++ pkg/helper/config.go
@@ -1,10 +1,7 @@
 package helper
 
-import "github.com/sirupsen/logrus"
-
 type Config struct {
 	Interface           string
 	LogLevel            string
 	WireGuardConfigPath string
-	log                 *logrus.Entry
 }
