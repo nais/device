3b1a6b1a0f73157e040233139d56999995580ad1
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-11-21 17:26:02 +0100

    add svg icons
    these should be used as source for making other formats

--- assets/icon_unix.go
+++ assets/icon_unix.go
@@ -6,16 +6,16 @@ package assets
 import _ "embed"
 
 var (
-	//go:embed nais-logo-blue.png
+	//go:embed nais-logo-blue@96x96.png
 	NaisLogoBlue []byte
 	//go:embed nais-logo-bw-connected.png
 	NaisLogoBwConnected []byte
 	//go:embed nais-logo-bw-disconnected.png
 	NaisLogoBwDisconnected []byte
-	//go:embed nais-logo-green.png
+	//go:embed nais-logo-green@96x96.png
 	NaisLogoGreen []byte
-	//go:embed nais-logo-red.png
+	//go:embed nais-logo-red@96x96.png
 	NaisLogoRed []byte
-	//go:embed nais-logo-yellow.png
+	//go:embed nais-logo-yellow@96x96.png
 	NaisLogoYellow []byte
 )
