1e8a2dd501ecac456cf649abf2b8e3ae972cd621
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2023-12-22 13:08:56 +0100

    add tests for disconnected state
    Co-authored-by: Morten Lied Johansen <morten.lied.johansen@nav.no>

---
+++ internal/device-agent/states/disconnected/disconnected_test.go
@@ -0,0 +1,55 @@
+package disconnected_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/nais/device/internal/device-agent/auth"
+	"github.com/nais/device/internal/device-agent/config"
+	"github.com/nais/device/internal/device-agent/runtimeconfig"
+	"github.com/nais/device/internal/device-agent/statemachine"
+	"github.com/nais/device/internal/device-agent/states/disconnected"
+	"github.com/nais/device/internal/pb"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDisconnected(t *testing.T) {
+	t.Run("disconnected waits for more events", func(t *testing.T) {
+		rc := runtimeconfig.NewMockRuntimeConfig(t)
+		var token *auth.Tokens
+		rc.EXPECT().SetToken(token)
+		rc.EXPECT().ResetEnrollConfig()
+
+		cfg := config.Config{
+			AgentConfiguration: &pb.AgentConfiguration{
+				AutoConnect: false,
+			},
+		}
+		state := disconnected.New(rc, cfg)
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		event := state.Enter(ctx)
+		assert.Equal(t, statemachine.EventWaitForExternalEvent, event)
+	})
+
+	t.Run("disconnected auto logins if configured (once)", func(t *testing.T) {
+		rc := runtimeconfig.NewMockRuntimeConfig(t)
+		var token *auth.Tokens
+		rc.EXPECT().SetToken(token)
+		rc.EXPECT().ResetEnrollConfig()
+
+		cfg := config.Config{
+			AgentConfiguration: &pb.AgentConfiguration{
+				AutoConnect: true,
+			},
+		}
+		state := disconnected.New(rc, cfg)
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		assert.Equal(t, statemachine.EventLogin, state.Enter(ctx))
+		assert.Equal(t, statemachine.EventWaitForExternalEvent, state.Enter(ctx))
+	})
+
+}
