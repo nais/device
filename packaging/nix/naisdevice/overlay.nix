5ad63e1cae270c6ddb524f8d5aa23f732617a0f9
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-07-08 13:15:18 +0200

    bump go to 1.22.5

--- packaging/nix/naisdevice/overlay.nix
+++ packaging/nix/naisdevice/overlay.nix
@@ -1,6 +1,6 @@
 let
-  goVersion = "1.22.4";
-  goSha256 = "sha256-/tcgZ45yinyjC6jR3tHKr+J9FgKPqwIyuLqOIgCPt4Q=";
+  goVersion = "1.22.5";
+  goSha256 = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
 in
   _final: prev: {
     go = prev.go.overrideAttrs (_old: {
