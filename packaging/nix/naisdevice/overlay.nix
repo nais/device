a15a7c159682164a3bfb52be7c844ba1bda961ea
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2024-09-17 11:45:10 +0200

    no overlay needed

--- packaging/nix/naisdevice/overlay.nix
+++ packaging/nix/naisdevice/overlay.nix
@@ -1,13 +1,13 @@
 let
-  goVersion = "1.22.5";
-  goSha256 = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
+  # goVersion = "1.22.5";
+  # goSha256 = "sha256-rJxyPyJJaa7mJLw0/TTJ4T8qIS11xxyAfeZEu0bhEvY=";
 in
   _final: prev: {
-    go = prev.go.overrideAttrs (_old: {
-      version = goVersion;
-      src = prev.fetchurl {
-        url = "https://go.dev/dl/go${goVersion}.src.tar.gz";
-        hash = goSha256;
-      };
-    });
+    # go = prev.go.overrideAttrs (_old: {
+    #   version = goVersion;
+    #   src = prev.fetchurl {
+    #     url = "https://go.dev/dl/go${goVersion}.src.tar.gz";
+    #     hash = goSha256;
+    #   };
+    # });
   }
