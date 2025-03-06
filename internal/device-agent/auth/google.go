18192bfa287c8537fbc1374c63d82f9416eda99b
Thomas Krampl <85170275+thokra-nav@users.noreply.github.com>
2025-03-06 10:14:39 +0100

    Redirect user to Console on successfull login (#405)
    * Redirect user to Console on successfull login
    
    It will fall back to kek when we cannot decide on correct url
    
    * Upgrade to latest Go 1.23 to fix vulnerabilities

--- internal/device-agent/auth/google.go
+++ internal/device-agent/auth/google.go
@@ -8,7 +8,9 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/lestrrat-go/jwx/jwt"
 	codeverifier "github.com/nirasan/go-oauth-pkce-code-verifier"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 )
 
@@ -73,8 +75,51 @@ func handleRedirectGoogle(state, redirectURI string, codeVerifier *codeverifier.
 			return
 		}
 
-		successfulResponse(w, "Successfully authenticated 👌 Close me pls", r.Header.Get("user-agent"))
+		ret, err := consoleURL(ctx, exchangeResponse.IDToken, "connected")
+		if err != nil {
+			logrus.Println("Failed to get console	URL: " + err.Error())
+			successfulResponse(w, "Successfully authenticated 👌 Close me pls", r.Header.Get("user-agent"))
+		} else {
+			http.Redirect(w, r, ret, http.StatusSeeOther)
+		}
+
 		tokens := &Tokens{Token: exchangeResponse.Token, IDToken: exchangeResponse.IDToken}
 		authFlowChan <- &authFlowResponse{Tokens: tokens, err: nil}
 	}
+}
+
+func consoleURL(ctx context.Context, idToken, state string) (string, error) {
+	// Parse id token to get domain
+	t, err := jwt.ParseString(idToken)
+	if err != nil {
+		return "", err
+	}
+	hd, _ := t.Get("hd")
+	domain, _ := hd.(string)
+
+	if domain == "" {
+		return "", fmt.Errorf("could not find domain in id token")
+	}
+
+	url := fmt.Sprintf("https://storage.googleapis.com/nais-tenant-data/%s.json", domain)
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	d := struct {
+		ConsoleURL string `json:"consoleUrl"`
+	}{}
+	err = json.NewDecoder(resp.Body).Decode(&d)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("https://%s?naisdevice=%s", d.ConsoleURL, state), nil
 }
