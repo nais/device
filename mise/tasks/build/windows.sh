b614f3e118b2c27a129cf9af4c27fd0b70086fcc
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-08-27 14:18:49 +0200

    build: sign windows binaries in packaging step

--- mise/tasks/build/windows.sh
+++ mise/tasks/build/windows.sh
@@ -8,8 +8,3 @@ GOOS="" GOARCH="" go tool github.com/akavel/rsrc -ico assets/nais-logo-blue.ico 
 go build -o bin/windows-client/naisdevice-systray.exe --tags "$GOTAGS" -ldflags "-s $LDFLAGS -H=windowsgui" ./cmd/naisdevice-systray
 go build -o bin/windows-client/naisdevice-agent.exe --tags "$GOTAGS" -ldflags "-s $LDFLAGS -H=windowsgui" ./cmd/naisdevice-agent
 go build -o bin/windows-client/naisdevice-helper.exe --tags "$GOTAGS" -ldflags "-s $LDFLAGS" ./cmd/naisdevice-helper
-
-# TODO: move somewhere else?
-./packaging/windows/sign-exe bin/windows-client/naisdevice-systray.exe ./packaging/windows/naisdevice.crt ./packaging/windows/naisdevice.key
-./packaging/windows/sign-exe bin/windows-client/naisdevice-agent.exe ./packaging/windows/naisdevice.crt ./packaging/windows/naisdevice.key
-./packaging/windows/sign-exe bin/windows-client/naisdevice-helper.exe ./packaging/windows/naisdevice.crt ./packaging/windows/naisdevice.key
