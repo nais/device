d0e61dbc4b0c468396753304634d24a049db082b
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-08 12:04:25 +0200

    build: start rewrite stuff to use nfpm, along with other changes to how we run scripts

--- mise/tasks/clean.sh
+++ mise/tasks/clean.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 #MISE description="Clean up build artifacts"
-rm -rf ./*.deb
+rm -f ./*.deb ./*.pkg
+rm -rf ./*.app
 rm -rf wireguard-go-*
 rm -rf wireguard-tools-*
-rm -rf naisdevice.app
-rm -f naisdevice-*.pkg
-rm -f naisdevice-*.deb
 rm -f ./packaging/windows/naisdevice*.exe
 rm -rf ./bin
-rm -rf ./packaging/*/icons
-rm -rf ./packaging/*/assets
--- mise/tasks/thirdparty/wireguard.sh
+++ mise/tasks/thirdparty/wireguard.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 #MISE description="Build WireGuard for MacOS client"
-mkdir -p bin/macos-client
-curl -L https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-1.0.20250521.tar.xz | tar xJ
-cd wireguard-tools-*/src && make && cp wg ../../bin/macos-client/
-rm -rf ./wireguard-tools-*
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+build_dir="$(mktemp -d)"
+trap 'echo "Removing tmp dir $build_dir" && rm -rf "$build_dir"' EXIT
+
+curl --location https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-1.0.20250521.tar.xz | \
+  tar --extract --xz --directory "$build_dir" --strip-components=1
+make --directory "$build_dir/src"
+mkdir --parents bin/macos-client
+cp "$build_dir/src/wg" ./bin/macos-client/
