fd25df0c6998c6e6eb9d5228b469da74a2d70ed9
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-08 12:17:45 +0200

    build: use short option

--- mise/tasks/thirdparty/wireguard-go.sh
+++ mise/tasks/thirdparty/wireguard-go.sh
@@ -11,5 +11,5 @@ trap 'echo "Removing tmp dir $build_dir" && rm -rf "$build_dir"' EXIT
 curl --location https://git.zx2c4.com/wireguard-go/snapshot/wireguard-go-0.0.20250522.tar.xz | \
   tar --extract --xz --directory "$build_dir" --strip-components=1
 make --directory "$build_dir"
-mkdir --parents bin/macos-client
+mkdir -p ./bin/macos-client
 cp "$build_dir/wireguard-go" ./bin/macos-client/
--- mise/tasks/thirdparty/wireguard.sh
+++ mise/tasks/thirdparty/wireguard.sh
@@ -11,5 +11,5 @@ trap 'echo "Removing tmp dir $build_dir" && rm -rf "$build_dir"' EXIT
 curl --location https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-1.0.20250521.tar.xz | \
   tar --extract --xz --directory "$build_dir" --strip-components=1
 make --directory "$build_dir/src"
-mkdir --parents bin/macos-client
+mkdir -p ./bin/macos-client
 cp "$build_dir/src/wg" ./bin/macos-client/
