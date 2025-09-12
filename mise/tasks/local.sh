09f4f4d03712fa62a7b3e37eeacaec8f1c292424
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-12 15:34:08 +0200

    fix: local run for google

--- mise/tasks/local.sh
+++ mise/tasks/local.sh
@@ -11,16 +11,32 @@ window_name="naisdevice-$(date +"%H%M%S")"
 enrollments_port=8081
 enrollments_url="http://localhost:${enrollments_port}/enrollments"
 
+auth="google"
+
 env=(
 	"ENROLLER_LOCALLISTENADDR=\":${enrollments_port}\""
-	"ENROLLER_GOOGLEENABLED=\"true\""
 	"APISERVER_WIREGUARDIP=\"127.0.0.1/24\""
 	"APISERVER_WIREGUARDIPV6=\"::1/64\""
 	"APISERVER_AUTOENROLLENABLED=\"true\""
 	"APISERVER_AUTOENROLLMENTSURL=\"${enrollments_url}\""
-	"APISERVER_DEVICEAUTHENTICATIONPROVIDER=\"google\""
 	"APISERVER_LOGLEVEL=\"debug\""
 )
+
+if [[ $auth == "google" ]]; then
+	env+=(
+		"ENROLLER_GOOGLE_ALLOWEDDOMAINS=\"dev-nais.io\""
+		"APISERVER_GOOGLE_ALLOWEDDOMAINS=\"dev-nais.io\""
+		"ENROLLER_GOOGLEENABLED=\"true\""
+		"APISERVER_DEVICEAUTHENTICATIONPROVIDER=\"google\""
+	)
+	tags="-tags tenant"
+else
+	env+=(
+		"ENROLLER_AZUREENABLED=\"true\""
+		"APISERVER_DEVICEAUTHENTICATIONPROVIDER=\"azure\""
+	)
+	tags=""
+fi
 
 # shellcheck disable=SC2116
 env_joined="$(IFS=' ' echo "${env[*]}")"
@@ -38,7 +54,7 @@ window_id="${session_id}:${window_name}"
 declare -A panes=(
 	[apiserver]="go run ./cmd/apiserver"
 	[enroller]="go run ./cmd/enroller"
-	[deviceagent]="go run ./cmd/naisdevice-agent --no-helper --custom-enroll-url 'http://localhost:8080/enroll'"
+	[deviceagent]="go run $tags ./cmd/naisdevice-agent --no-helper --custom-enroll-url 'http://localhost:8080/enroll'"
 )
 
 for pane in "${!panes[@]}"; do
