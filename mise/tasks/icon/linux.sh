b92fd6faf7aad703f1bba02b1825e84418bb5974
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-18 14:49:19 +0200

    build: fix linux icons

--- mise/tasks/icon/linux.sh
+++ mise/tasks/icon/linux.sh
@@ -5,7 +5,8 @@ for size in 16x16 32x32 64x64 128x128 256x256 512x512; do
 	mkdir -p "$out/$size/apps/"
 	convert -background none \
 		assets/icon/src/blue.svg \
-		-gravity center \
 		-resize "$size" \
+		-gravity center \
+		-extent "$size" \
 		"$out/$size/apps/naisdevice.png"
 done
