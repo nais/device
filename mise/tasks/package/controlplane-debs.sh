c0f671594c2f4b4dceba4bc74b17537944735878
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-19 14:47:16 +0200

    build: fix controlplane packaging

--- mise/tasks/package/controlplane-debs.sh
+++ mise/tasks/package/controlplane-debs.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 #MISE description="Package controlplane-debs"
-for packager in ./packaging/controlplane/*/build-deb; do
-	$packager "$VERSION"
-done
+#MISE depends=["build:controlplane"]
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+version="$(date "+%Y-%m-%d-%H%M%S")"
+VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/apiserver/nfpm.yaml --target apiserver.deb
+VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/gateway-agent/nfpm.yaml --target gateway-agent.deb
+VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/prometheus-agent/nfpm.yaml --target prometheus-agent.deb
