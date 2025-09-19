9c4ac0f1370fa0a68afe0293d2066afcb7972c2e
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-19 15:26:01 +0200

    build(controlplane): set version properly

--- mise/tasks/package/controlplane-debs.sh
+++ mise/tasks/package/controlplane-debs.sh
@@ -6,7 +6,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-version="$(date "+%Y-%m-%d-%H%M%S")"
-VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/apiserver/nfpm.yaml --target apiserver.deb
-VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/gateway-agent/nfpm.yaml --target gateway-agent.deb
-VERSION="$version" go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/prometheus-agent/nfpm.yaml --target prometheus-agent.deb
+go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/apiserver/nfpm.yaml --target apiserver.deb
+go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/gateway-agent/nfpm.yaml --target gateway-agent.deb
+go tool github.com/goreleaser/nfpm/v2/cmd/nfpm package --packager deb --config ./packaging/controlplane/prometheus-agent/nfpm.yaml --target prometheus-agent.deb
