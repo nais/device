7a2ccb76098544682beb64528215d8f3495a5b3f
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-22 11:20:58 +0200

    build: remove dependency on release-info

--- mise/tasks/ci/create-package-manager-pr.sh
+++ mise/tasks/ci/create-package-manager-pr.sh
@@ -5,8 +5,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# shellcheck disable=SC2153
-version="$VERSION"
 workspace="$MISE_PROJECT_ROOT"
 
 repo="$1"
@@ -21,12 +19,16 @@ gh repo clone "$repo" "$repo_dir" -- --depth=1
 echo "repo cloned"
 cd "$repo_dir" || exit 1
 
+vars="$workspace/template.vars"
+
 # generate file from template
 # shellcheck disable=SC2046
-env $(xargs -0 <"$workspace/template.vars") \
-	envsubst "$(sed 's/^/$/' "$workspace/template.vars")" \
+env $(xargs -0 <"$vars") \
+	envsubst "$(sed 's/^/$/' "$vars")" \
 	<"$workspace/.github/workflows/templates/${basename}" \
 	>"${file}"
+
+version=$(grep -m 1 -oP '^VERSION=.+$' "$vars" | cut -d '=' -f 2)
 
 # create pr
 git config user.name "NAIS team app"
