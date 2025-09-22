971af677eac4fd77fe7dacdf4504921192d4269e
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-09-22 10:33:10 +0200

    build: fail release-info when no changelog
    also try to make the script more readable

--- mise/tasks/ci/release-info.sh
+++ mise/tasks/ci/release-info.sh
@@ -13,43 +13,43 @@ main() {
 		--unreleased \
 		--verbose \
 		--strip all)"
-	output "changelog" "$changelog"
 
-	version="$(grep -m 1 -oP '(?<=^##\s)v\d+\.\d+\.\d+(?=\s-\s)' <<<"$changelog")"
-	if [[ -z "$version" ]]; then
-		echo "Unable to read version from changelog, abort"
-		return
-	fi
-	output "version" "$version"
-
-	{
-		if [[ -n "$changelog" ]]; then
+	local body
+	if [[ -n "$changelog" ]]; then
+		body=$(
 			cat <<-EOF
 				# :pencil: Changelog preview
 				Below is a preview of the Changelog that will be added to the next release. Only commit messages that follow the [Conventional Commits specification](https://www.conventionalcommits.org/) will be included in the Changelog.
 
 				$changelog
 			EOF
-		else
+		)
+	else
+		body=$(
 			cat <<-EOF
-				# :disappointed: No release for you
+				body="# :disappointed: No release for you
 				There are no commits in your branch that follow the [Conventional Commits specification](https://www.conventionalcommits.org/), so no release will be created.
 
 				If you want to create a release from this pull request, please reword your commit messages to replace this message with a preview of a beautiful Changelog."
 			EOF
-		fi
-	} | if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
-		gh pr comment "${3:-${GITHUB_REF_NAME%%/merge}}" \
+		)
+	fi
+
+	if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+		gh pr comment "${GITHUB_REF_NAME%%/merge}" \
 			--edit-last --create-if-none \
 			--repo "$repository" \
-			--body-file=-
-		# else
-		# TODO: Decide if we want this outside of PRs.
-		# echo
-		# echo "This would have been posted if you ran this on GitHub:"
-		# cat
+			--body "$body"
 	fi
-	# common functions for CI tasks
+
+	version="$(grep -m 1 -oP '(?<=^##\s)v\d+\.\d+\.\d+(?=\s-\s)' <<<"$changelog")"
+	if [[ -z "$version" ]]; then
+		echo "Unable to read version from changelog, abort"
+		exit 1
+	fi
+
+	output "changelog" "$changelog"
+	output "version" "$version"
 }
 
 output() {
