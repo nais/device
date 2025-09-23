70f5e9cce99e910aa99b4829b2e76617252e2755
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-23 13:12:33 +0200

    build: default to empty GOTAGS

--- mise/tasks/build/linux.sh
+++ mise/tasks/build/linux.sh
@@ -6,9 +6,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+gotags="${GOTAGS:-""}"
+
 ldflags="-X github.com/nais/device/internal/version.Version=${VERSION:-local} -X github.com/nais/device/internal/otel.endpointURL=https://collector-internet.nav.cloud.nais.io"
 mkdir -p ./bin/linux-client
 
-go build -o bin/linux-client/naisdevice-systray --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
-go build -o bin/linux-client/naisdevice-agent --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-agent
-go build -o bin/linux-client/naisdevice-helper --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
+go build -o bin/linux-client/naisdevice-systray --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
+go build -o bin/linux-client/naisdevice-agent --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-agent
+go build -o bin/linux-client/naisdevice-helper --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
--- mise/tasks/build/windows.sh
+++ mise/tasks/build/windows.sh
@@ -6,12 +6,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+gotags="${GOTAGS:-""}"
+
 ldflags="-X github.com/nais/device/internal/version.Version=${VERSION:-local} -X github.com/nais/device/internal/otel.endpointURL=https://collector-internet.nav.cloud.nais.io"
 mkdir -p ./bin/windows-client
 
 REALGOARCH="${GOARCH:-amd64}"
 GOOS="" GOARCH="" go tool github.com/akavel/rsrc -arch "$REALGOARCH" -manifest ./assets/windows/admin_manifest.xml -ico ./assets/windows/icon/naisdevice.ico -o ./cmd/naisdevice-helper/main_windows.syso
 GOOS="" GOARCH="" go tool github.com/akavel/rsrc -arch "$REALGOARCH" -ico assets/windows/icon/naisdevice.ico -o ./cmd/naisdevice-agent/main_windows.syso
-go build -o bin/windows-client/naisdevice-systray.exe --tags "$GOTAGS" -ldflags "-s $ldflags -H=windowsgui" ./cmd/naisdevice-systray
-go build -o bin/windows-client/naisdevice-agent.exe --tags "$GOTAGS" -ldflags "-s $ldflags -H=windowsgui" ./cmd/naisdevice-agent
-go build -o bin/windows-client/naisdevice-helper.exe --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
+go build -o bin/windows-client/naisdevice-systray.exe --tags "$gotags" -ldflags "-s $ldflags -H=windowsgui" ./cmd/naisdevice-systray
+go build -o bin/windows-client/naisdevice-agent.exe --tags "$gotags" -ldflags "-s $ldflags -H=windowsgui" ./cmd/naisdevice-agent
+go build -o bin/windows-client/naisdevice-helper.exe --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
