c8123a9ba0c1ff4aeda1d7066327cd8d9b9cb512
Christer Edvartsen <christer.edvartsen@nav.no>
2025-09-23 13:12:33 +0200

    build: default to empty GOTAGS

--- mise/tasks/build/macos.sh
+++ mise/tasks/build/macos.sh
@@ -6,9 +6,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+gotags="${GOTAGS:-""}"
+
 ldflags="-X github.com/nais/device/internal/version.Version=${VERSION:-local} -X github.com/nais/device/internal/otel.endpointURL=https://collector-internet.nav.cloud.nais.io"
 mkdir -p ./bin/macos-client
 
-go build -o bin/macos-client/naisdevice-agent --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-agent
-CGO_ENABLED=1 go build -o bin/macos-client/naisdevice-systray --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
-go build -o bin/macos-client/naisdevice-helper --tags "$GOTAGS" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
+go build -o bin/macos-client/naisdevice-agent --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-agent
+CGO_ENABLED=1 go build -o bin/macos-client/naisdevice-systray --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
+go build -o bin/macos-client/naisdevice-helper --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
