2a8126ad3e9da686326032b1d94433921b4e07b5
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2025-10-10 10:48:23 +0200

    feat: support macos 10.14

--- mise/tasks/build/macos.sh
+++ mise/tasks/build/macos.sh
@@ -12,5 +12,8 @@ ldflags="-X github.com/nais/device/internal/version.Version=${VERSION:-local} -X
 mkdir -p ./bin/macos-client
 
 go build -o bin/macos-client/naisdevice-agent --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-agent
-CGO_ENABLED=1 go build -o bin/macos-client/naisdevice-systray --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
+CGO_ENABLED=1 \
+	CGO_CFLAGS="-mmacosx-version-min=10.14" \
+	CGO_LDFLAGS="-mmacosx-version-min=10.14" \
+	go build -o bin/macos-client/naisdevice-systray --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-systray
 go build -o bin/macos-client/naisdevice-helper --tags "$gotags" -ldflags "-s $ldflags" ./cmd/naisdevice-helper
