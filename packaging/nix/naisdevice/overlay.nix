0b097f81d8417845f097c276474165bb8dbb76be
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-02-16 12:50:26 +0100

    build: bump go

--- packaging/nix/naisdevice/overlay.nix
+++ packaging/nix/naisdevice/overlay.nix
@@ -1,17 +1,18 @@
-final: prev:
-let
-  goVersion = "1.25.3";
+final: prev: let
+  goVersion = "1.26.0";
   newerGoVersion = prev.go.overrideAttrs (old: {
     inherit goVersion;
     src = prev.fetchurl {
       url = "https://go.dev/dl/go${goVersion}.src.tar.gz";
-      hash = "sha256-qBpLpZPQAV4QxR4mfeP/B8eskU38oDfZUX0ClRcJd5U=";
+      hash = "sha256-yRMqih9r0qpKrR10uCMdlSdJUEg6SVBlfubFbm6Bd5A=";
     };
   });
   nixpkgsVersion = prev.go.version;
   newVersionNotInNixpkgs = -1 == builtins.compareVersions nixpkgsVersion goVersion;
-in
-{
-  go = if newVersionNotInNixpkgs then newerGoVersion else prev.go;
-  buildGoModule = prev.buildGoModule.override { go = final.go; };
+in {
+  go =
+    if newVersionNotInNixpkgs
+    then newerGoVersion
+    else prev.go;
+  buildGoModule = prev.buildGoModule.override {go = final.go;};
 }
