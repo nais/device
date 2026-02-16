abbc0231542a8428e07e1561a70ae586b530cbba
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-02-16 12:50:26 +0100

    build: bump go

--- flake.nix
+++ flake.nix
@@ -14,62 +14,58 @@
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.flake-root.flakeModule ];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.flake-root.flakeModule];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
-      perSystem =
-        {
-          config,
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              (import ./packaging/nix/naisdevice/overlay.nix) # Allows referencing new versions of go before landed in channel
-            ];
-          };
-          packages.default = config.packages.naisdevice;
-          packages.naisdevice = pkgs.callPackage ./packaging/nix/naisdevice/package.nix {
-            inherit (inputs) self;
-          };
-          packages.apiserver = pkgs.callPackage ./packaging/nix/controlplane/package.nix {
-            inherit (inputs) self;
-            subPackage = "apiserver";
-          };
-          checks.naisdevice = pkgs.callPackage ./packaging/nix/naisdevice/test.nix {
-            inherit (config.packages) naisdevice;
-          };
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              gcc # needed for sqlite3-go
-              gnumake
-              go
-              gopls
-              graphviz
-              imagemagick
-              protobuf
-              sqlite-interactive # -interactive gives readline / ncurses
-            ];
-          };
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            # (import ./packaging/nix/naisdevice/overlay.nix) # Allows referencing new versions of go before landed in channel
+          ];
         };
+        packages.default = config.packages.naisdevice;
+        packages.naisdevice = pkgs.callPackage ./packaging/nix/naisdevice/package.nix {
+          inherit (inputs) self;
+        };
+        packages.apiserver = pkgs.callPackage ./packaging/nix/controlplane/package.nix {
+          inherit (inputs) self;
+          subPackage = "apiserver";
+        };
+        checks.naisdevice = pkgs.callPackage ./packaging/nix/naisdevice/test.nix {
+          inherit (config.packages) naisdevice;
+        };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc # needed for sqlite3-go
+            gnumake
+            go
+            gopls
+            graphviz
+            imagemagick
+            protobuf
+            sqlite-interactive # -interactive gives readline / ncurses
+          ];
+        };
+      };
       flake = {
-        gce =
-          let
-            system = "x86_64-linux";
-          in
+        gce = let
+          system = "x86_64-linux";
+        in
           inputs.nixos-generators.nixosGenerate {
             inherit system;
-            modules = [ ./packaging/nix/controlplane/module.nix ];
+            modules = [./packaging/nix/controlplane/module.nix];
             specialArgs = {
               package = inputs.self.packages.${system}.apiserver;
             };
@@ -77,13 +73,12 @@
           };
         nixosModules = rec {
           default = naisdevice;
-          naisdevice =
-            {
-              pkgs,
-              lib,
-              config,
-              ...
-            }:
+          naisdevice = {
+            pkgs,
+            lib,
+            config,
+            ...
+          }:
             import ./packaging/nix/naisdevice/module.nix {
               inherit (inputs) self;
               inherit
