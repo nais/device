200d796a75d15437f23ba9705b4ea1f463af3797
x10an14-nav <christian.chavez@nav.no>
2026-03-19 00:23:35 +0100

    flake: update inputs

--- packaging/nix/naisdevice/overlay.nix
+++ packaging/nix/naisdevice/overlay.nix
@@ -4,7 +4,7 @@ final: prev: let
     inherit goVersion;
     src = prev.fetchurl {
       url = "https://go.dev/dl/go${goVersion}.src.tar.gz";
-      hash = "sha256-yRMqih9r0qpKrR10uCMdlSdJUEg6SVBlfubFbm6Bd5A=";
+      hash = "sha256-MXIpPQSyCdwRRGmOe6E/BHf2uoxf/QvmbCD9vJeF37s=";
     };
   });
   nixpkgsVersion = prev.go_latest.version;
