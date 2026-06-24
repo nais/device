60c22a5d4fbaabaebba408181a198d9d593ff39f
x10an14-nav <98316196+x10an14-nav@users.noreply.github.com>
2026-06-24 10:24:42 +0200

    nix: await staging release of new go version (#541)

--- packaging/nix/naisdevice/overlay.nix
+++ packaging/nix/naisdevice/overlay.nix
@@ -1,18 +1,17 @@
-final: prev: let
-  goVersion = "1.26.3";
+final: prev:
+let
+  goVersion = "1.26.4";
   newerGoVersion = prev.go_latest.overrideAttrs (old: {
     inherit goVersion;
     src = prev.fetchurl {
       url = "https://go.dev/dl/go${goVersion}.src.tar.gz";
-      hash = "sha256-HGRoddCqh5kTMYTtV895/yS97+jIggRwYCqdPW2Rkrg=";
+      hash = "sha256-T2aKMvv8ETLmqIH7lowvHa2mMUkqM5IRc1+7JVpCYC0=";
     };
   });
   nixpkgsVersion = prev.go_latest.version;
   newVersionNotInNixpkgs = -1 == builtins.compareVersions nixpkgsVersion goVersion;
-in {
-  go_latest =
-    if newVersionNotInNixpkgs
-    then newerGoVersion
-    else prev.go_latest;
-  buildGoLatestModule = prev.buildGoLatestModule.override {go = final.go_latest;};
+in
+{
+  go_latest = if newVersionNotInNixpkgs then newerGoVersion else prev.go_latest;
+  buildGoLatestModule = prev.buildGoLatestModule.override { go = final.go_latest; };
 }
